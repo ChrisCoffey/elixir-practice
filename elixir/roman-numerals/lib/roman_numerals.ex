5c8cf985e98a2af7d1e1b19d19c0f379e9126096
Chris Coffey <cmcof273@gmail.com>
2020-08-09 10:15:07 -0400

    Roman numeral conversion

---
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -0,0 +1,40 @@
+defmodule RomanNumerals do
+
+  @doc """
+  Convert the number to a roman number.
+  """
+  @spec numeral(pos_integer) :: String.t()
+  def numeral(number) do
+    conversionTable = %{
+      1000 => "M",
+      900 => "CM",
+      500 => "D",
+      400 => "CD",
+      100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+      }
+
+    # Start from the left and work down to the right
+    res = List.foldl(Enum.reverse(Map.keys(conversionTable)), {"", number}, fn elem, {acc, n} ->
+      counts = div(n, elem)
+      leftover = rem(n, elem)
+
+      if counts > 0 do
+        newChars = Enum.reduce(List.duplicate(conversionTable[elem], counts), fn l, r -> l <> r end)
+
+        {acc<>newChars, leftover}
+      else
+        {acc, leftover}
+      end
+    end
+    )
+    elem(res, 0)
+  end
+end
